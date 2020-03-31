6950ef1af57d0efaa805a3866c2fe6248f587f64
Armaan Rai <armaan@rai.com>
2020-03-30 17:23:06 -0700

    create basic ui for interval timer

--- app/client/src/App.js
+++ app/client/src/App.js
@@ -1,13 +1,62 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+let currentInterval = 0;
+let restMinutes = 0;
+let restSeconds = 0;
+let workMinutes = 0;
+let workSeconds = 0;
 
 function App() {
   return (
     <div className="App">
-      <h1>Time</h1>
+      <header>
+        <h1>Time</h1>
+      </header>
+      <div id='intervalTracker'>
+        <div>
+          <h2>Rest:</h2>
+          <input type="number" id="restMinutes"/><p>mins</p>
+          <input type="number" id="restSeconds"/><p>secs</p>
+        </div>
+        <br/>
+        <br/>
+        <div>
+          <h2>Work:</h2>
+          <input type="number" id="workMinutes"/><p>mins</p>
+          <input type="number" id="workSeconds"/><p>secs</p>
+        </div>
+        <br/>
+        <br/>
+        <div>
+          {displayClock()}
+        </div>
+        <button onClick={setInterval}>Start</button>
+      </div>
     </div>
   );
 }
+
+const displayClock = () => {
+  return <h1>{`${restMinutes}:${restSeconds}`}</h1>
+};
+
+const setInterval = () => {
+  restMinutes = document.getElementById('restMinutes');
+  restSeconds = document.getElementById('restSeconds');
+  workMinutes = document.getElementById('workMinutes');
+  workSeconds = document.getElementById('workSeconds');
+
+
+
+
+
+  console.error('======restMinutes=======>', restMinutes.value);
+  console.error('======restSeconds=======>', restSeconds.value);
+  console.error('======workMinutes=======>', workMinutes.value);
+  console.error('======workSeconds=======>', workSeconds.value);
+};
+
+
 
 export default App;
